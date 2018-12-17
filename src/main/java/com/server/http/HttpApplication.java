31efe5e95cdfbf2558a6b052329c9e8f495c29be
xubaodian <xubaodian@hikvision.com.cn>
2018-12-17 18:43:08 +0800

    test

--- src/main/java/com/server/http/HttpApplication.java
+++ src/main/java/com/server/http/HttpApplication.java
@@ -2,13 +2,10 @@ package com.server.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@ServletComponentScan
 public class HttpApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/server/http/controller/UserController.java
+++ src/main/java/com/server/http/controller/UserController.java
@@ -1,10 +1,16 @@
-package com.server.http;
+package com.server.http.controller;
 
 import com.server.http.entity.AjaxReult;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(value = "/user")
@@ -21,5 +27,18 @@ public class UserController {
             ajaxReult.setMessage("用户名或密码错误");
         }
         return ajaxReult;
+    }
+
+    @GetMapping(value = "/session")
+    public @ResponseBody String login(HttpServletRequest req, HttpServletResponse resp) {
+        System.out.println(req.getSession().getAttribute("token"));
+        req.getSession().setAttribute("token", "12345612345");
+        return  "ok";
+    }
+
+    @GetMapping(value = "logout")
+    public @ResponseBody String logout(HttpSession httpSession){
+        httpSession.invalidate();
+        return "logout";
     }
 }
