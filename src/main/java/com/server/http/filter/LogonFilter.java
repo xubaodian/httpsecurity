2cf215c8de1f9bb41f0e84dbeee6f8a9ad1c56f7
xubaodian <xubaodian@hikvision.com.cn>
2018-12-18 11:17:30 +0800

    一些学习代码

--- src/main/java/com/server/http/filter/LogonFilter.java
+++ src/main/java/com/server/http/filter/LogonFilter.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 @WebFilter(filterName="CustomFilter",urlPatterns="/*")
 public class LogonFilter implements Filter {
 
+    //不拦截url列表
     private static final String [] ignoreUrls = {"/login", "/js/"};
 
     @Override
